e5a59d36a9bf7e5d9421dae5e3255e6be1cbc6f3
vampire <zixinq@jumei.com>
2016-05-10 11:10:03 +0800

    add swift test

---
+++ SwiftTest/MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+print("abc")
+
+
+let language = "swift"
+
+print("the lauguage name is \(language)")
+
+/* comment like this
+  this is still a comment */
+
+
+let minValue = UInt8.min
+
+let abv = 3.1415926
+
+
+let a = 1.25e2
+let b = 1.25e-2
+b.hashValue
+
+
+/*  type alias */
+typealias AudioSample = UInt16
+
+let c : AudioSample = 5
+
+
+let http404Error = (404,"not found")
+print(http404Error.0)
+
+
+let (code, error) = http404Error
+print("code is \(code)   and error is \(error)")
+
+
+
+
---
+++ SwiftTest/VendingMachine.swift
@@ -0,0 +1,20 @@
+//
+//  VendingMachine.swift
+//  SwiftTest
+//
+//  Created by admin on 16/5/9.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+import UIKit
+
+struct Item {
+    var price : Int
+    var count : Int
+}
+
+
+class VendingMachine: NSObject {
+
+       
+}
