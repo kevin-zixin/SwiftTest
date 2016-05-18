c00c21c1d59a344d003410758f7bb18a59cd3340
vampire <zixinq@jumei.com>
2016-05-18 15:08:56 +0800

    add new

--- SwiftTest/MyPlayground.playground/Contents.swift
+++ SwiftTest/MyPlayground.playground/Contents.swift
@@ -40,5 +40,187 @@ let (code, error) = http404Error
 print("code is \(code)   and error is \(error)")
 
 
+let letter = (a,b,"c")
+let (first,_,_)=letter
+print("first is ",first)
+
+
+print(http404Error.0)
+
+
+let httpError = (code:404,des:"not found")
+
+print(httpError.code)
+
+
+let possibleNum = "1234567"
+let convertedNum = Int(possibleNum)
+
+if convertedNum != nil {
+    print("has value")
+}
+
+if let optionNum = Int(possibleNum){
+    print("has num",optionNum)
+}else{
+    print("no num")
+}
+
+if let firstnumber = Int("a"), secondnumber = Int("6") where firstnumber < secondnumber {
+    print("\(firstnumber) < \(secondnumber)")
+}else{
+    print("no")
+}
+
+
+func canThrowAnError() throws {
+    
+}
+
+
+do {
+    try canThrowAnError()
+} catch {
+    
+}
+
+let age = 3 /*age = -3 */
+assert(age >= 0, "age can not less than 0")
+
+8%2.5
+8%(-2.5)
+var h = 0
+h+=1
+let m=h
+
+
+(3,"apple") < (3,"bird")
+
+(3,"aec") > (3,"adc")
+
+
+let q=99,w=100
+var e = 0
+
+let r = (q > w ? 59:20)
+
+
+let defaultColorName = "red"
+var userDefinedColorName: String?
+
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+
+
+var welcome = "hello"
+welcome.insert("b", atIndex: welcome.endIndex)
+welcome.removeRange(welcome.endIndex.advancedBy(-1)..<welcome.endIndex)
+
+
+//var someInts = [Int] ()
+var someInts : [Int] = []
+
+someInts.append(1)
+someInts.append(2)
+
+var threeDoubles = [Double](count: 3, repeatedValue: 0.0)
+
+var airports : [String : String] = [:]
+var anotherAirports  = [String:String]()
+if anotherAirports.isEmpty{
+    print("empty")
+}
+anotherAirports["abc"] = "cde"
+anotherAirports["bcd"] = "def"
+anotherAirports
+
+
+
+for (name , valuename) in anotherAirports{
+    print("\(name)+\(valuename)")
+    
+    switch name {
+        case "abc":
+            print("1");
+        default :
+            print("2")
+    }
+    
+}
+
+let somePoint = (1,1)
+switch somePoint {
+    case (0,0):
+        print("1")
+default:
+    print("2")
+    
+}
+
+
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also"
+    fallthrough
+default:
+    description += " an integer."
+}
+
+
+func firstFunc(firstParameter : Int, secondParameter : Int) -> (a: Int , b: Int , c:Int) {
+    
+    return (firstParameter,secondParameter,firstParameter+secondParameter)
+}
+
+
+firstFunc(1, secondParameter: 2)
+
+
+func arithmeticMean(numbers: Double...) -> Double{
+    var total :Double = 0
+    for number in numbers {
+        total += number
+    }
+    
+    return total/Double(numbers.count)
+}
+
+
+func stepForward(input:Int) -> Int {
+    return input+1
+}
+
+func stepBackward(input:Int) -> Int {
+    return input-1
+}
+
+
+func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
+    return backwards ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveToZero = chooseStepFunction(currentValue>0)
+
+moveToZero(currentValue)
+
+
+func chooseStepFunction2(backwards : Bool) -> (Int)->Int {
+    func stepForward(input:Int) -> Int {
+        return input+1
+    }
+    
+    func stepBackward(input:Int) -> Int {
+        return input-1
+    }
+
+    return backwards ? stepBackward : stepForward
+}
+
+
+let goToZero = chooseStepFunction2(currentValue>0)
+
+goToZero(currentValue)
 
 
--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -7,17 +7,115 @@
 //
 
 import UIKit
-
+import CoreMotion
 class ViewController: UIViewController {
-
+    
+    var manager = CMMotionManager()
+    var activityManager = CMMotionActivityManager()
+    var pedometer = CMPedometer()
+    var mylabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mylabel.frame = CGRectMake(100, 200, 200, 100)
+        mylabel.textColor = UIColor.blueColor()
+        mylabel.text = "0"
+        self.view.addSubview(mylabel)
         // Do any additional setup after loading the view, typically from a nib.
+        testStep()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func testStep()  {
+        let calendar = NSCalendar.currentCalendar()
+        let components = NSDateComponents()
+        components.day = 9
+        components.month = 5
+        components.year = 2015
+        components.hour = 24
+        components.minute=0
+        
+        let startDate = calendar.dateFromComponents(components)
+        let endDate = NSDate()
+        
+        if CMPedometer.isStepCountingAvailable() {
+            pedometer.queryPedometerDataFromDate(startDate!, toDate: endDate, withHandler: { ( data :CMPedometerData?,error : NSError?) in
+                print(data?.numberOfSteps)
+                print(error)
+            })
+        }
+        
+        
+        if CMPedometer.isStepCountingAvailable() {
+            pedometer.startPedometerUpdatesFromDate(NSDate(), withHandler: { (data :CMPedometerData?,error : NSError?) in
+                print(data?.numberOfSteps)
+                self.mylabel.text = "\(data?.numberOfSteps)"
+            })
+        }
+    }
+    
+    
+    func testMotion () {
+        
+        
+        
+        manager.deviceMotionUpdateInterval = 1.0/10.0
+        if manager.accelerometerAvailable {
+            let queue = NSOperationQueue.currentQueue()
+            manager.startAccelerometerUpdatesToQueue(queue!, withHandler: { ( accelerometerData : CMAccelerometerData?, error : NSError?) in
+                if accelerometerData != nil {
+                    
+                    if accelerometerData?.acceleration.x>1.3{
+                        print(accelerometerData?.acceleration.x)
+                    }
+                    
+                    if accelerometerData?.acceleration.y>1.3{
+                        print(accelerometerData?.acceleration.y)
+                    }
+
+                    if accelerometerData?.acceleration.z>1.3{
+                        print(accelerometerData?.acceleration.z)
+                    }
+
+
+                }
+            })
+            
+            manager.startDeviceMotionUpdatesToQueue(queue!, withHandler: { ( motion : CMDeviceMotion?, error : NSError?) in
+                if motion != nil {
+                    
+                }
+            })
+            
+            
+            
+            
+            activityManager.startActivityUpdatesToQueue(queue!, withHandler: { (activity : CMMotionActivity?) in
+                
+                
+                if (activity!.unknown) {
+                   print("unknow")
+                } else if (activity!.walking) {
+                    print("walking")
+                } else if (activity!.running) {
+                    print("running")
+                } else if (activity!.automotive) {
+                    print("automotive")
+                } else if (activity!.stationary) {
+                    print("stationary")
+                }
+                
+
+            })
+            
+            
+        }
+        
     }
 
 
